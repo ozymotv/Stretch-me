9f8157e930042e7735e699a3db768677069561bd
Ozymo Alt <nvhai.contact@gmail.com>
2024-12-07 18:54:52 +0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,1 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
+#include <QMainWindow>
+#include <QSystemTrayIcon>
+#include "config.h"
+#include "resolutionmanager.h"
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Config* m_config;
+    ResolutionManager* m_resolutionManager;
+    QSystemTrayIcon* m_trayIcon;
+
+    void createTrayIcon();
+    void setupResolutionManager();
+};
+
+#endif // MAINWINDOW_H
