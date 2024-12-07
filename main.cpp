a5f3f0482b49e28c39bc024d828cb9bcf4f00350
Ozymo Alt <nvhai.contact@gmail.com>
2024-12-07 18:55:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
 
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    // Ensure only one instance
+    QApplication::setQuitOnLastWindowClosed(false);
+    
+    MainWindow window;
+    return app.exec();
+}
