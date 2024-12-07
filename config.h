6e93c6980513f5099ae947425aafd478102deefa
Ozymo Alt <nvhai.contact@gmail.com>
2024-12-07 18:53:16 +0700

    Update config.h

--- config.h
+++ config.h
@@ -1,1 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+#include <QMap>
+#include <QString>
+#include <QSettings>
+
+class Config {
+public:
+    Config(const QString& filename = "Options.ini");
+    
+    QString getResolution(const QString& title, const QString& executable);
+    void setResolution(const QString& key, const QString& resolution);
+    void save();
+
+private:
+    QSettings* settings;
+    void createDefaultConfig();
+};
+
+#endif // CONFIG_H
