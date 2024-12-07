7a36f939c04e060709029d2586e7ba66289900ab
Ozymo Alt <nvhai.contact@gmail.com>
2024-12-07 18:55:09 +0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,1 +1,40 @@
+#include "mainwindow.h"
+#include <QMenu>
+#include <QAction>
+#include <QApplication>
 
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent) {
+    
+    m_config = new Config();
+    m_resolutionManager = new ResolutionManager(m_config, this);
+    
+    createTrayIcon();
+    setupResolutionManager();
+    
+    // Start minimized
+    hide();
+    m_resolutionManager->startMonitoring();
+}
+
+MainWindow::~MainWindow() {
+    m_resolutionManager->stopMonitoring();
+}
+
+void MainWindow::createTrayIcon() {
+    QMenu* trayMenu = new QMenu(this);
+    
+    QAction* quitAction = new QAction("Quit", this);
+    connect(quitAction, &QAction::triggered, qApp, &QApplication::quit);
+    
+    trayMenu->addAction(quitAction);
+    
+    m_trayIcon = new QSystemTrayIcon(this);
+    m_trayIcon->setContextMenu(trayMenu);
+    m_trayIcon->setIcon(QIcon(":/icon.png")); // You'd need to add an app icon
+    m_trayIcon->show();
+}
+
+void MainWindow::setupResolutionManager() {
+    // Optional: Add configuration methods here
+}
